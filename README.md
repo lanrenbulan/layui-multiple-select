3eb45091fc32cb0aa874a7a8527feb56466b9cba
topwms <zhangsizhao@126.com>
2024-01-26 05:56:37 +0000

    功能增强

--- README.md
+++ README.md
@@ -1,75 +1,7 @@
 ## 介绍
 
-layui2.8，select多选，支持搜索
+layui2.9.*，select多选，支持搜索
 
 ## 使用示例
-```
-<div class="layui-container">
-  <form class="layui-form">
-    <div class="layui-form-item">
-      <label class="layui-form-label">城市</label>
-      <div class="layui-input-block">
-      <div id="select"></div>
-    </div>
-    </div>
-    <button class="layui-btn" lay-submit lay-filter="demo-verify">提交</button>
-    <button class="layui-btn" type="button" id="reload">重载</button>
-  </form>
-</div>
-```
-```
-layui.config({
-  // 模块地址
-  base: '/test/layui/'
-}).extend({
-  // 定义模块路径
-  select: 'select'
-});
-  
-layui.use(['select', 'form'], function(){
-  const select = layui.select;
-  const form = layui.form;
-  const $ = layui.$;
 
-  const citySelect = select.render({
-    elem: '#select',
-    // 表单名称
-    name: 'city',
-    options: [{id: 1,title: '北京'}, {id: 2,title: '上海'}, {id: 3,title: '湖北'}],
-    // 验证规则
-    verify: 'required',
-    // 初始值
-    value: [{id: 1,title: '北京'}, {id: 2,title: '上海'}]
-  });
-
-
-  $('#reload').click(function() {
-    // 重载
-    citySelect.reload({
-      options: [{id: 1,title: '北京'}, {id: 2,title: '上海'}, {id: 3,title: '湖北'}, {id: 4,title: '湖南'}],
-    });
-    // 传参：设置值
-    citySelect.val('1,4');
-    // 不传参，获取值
-    citySelect.val();
-    // 清除值
-    citySelect.reset();
-  });
-
-   // 提交事件
-  form.on('submit(demo-verify)', function(data){
-    var field = data.field; // 获取表单字段值
-    // 显示填写结果，仅作演示用
-    layer.alert(JSON.stringify(field), {
-      title: '当前填写的字段值'
-    });
-    // 此处可执行 Ajax 等操作
-    // …
-    return false; // 阻止默认 form 跳转
-  });
-});
-```
-
-## 示例图
-![image](https://github.com/lanrenbulan/layui-select-multiple/assets/9604624/a40a1233-19a1-45b2-8908-db119b139bdf)
-
+见index.html
--- select.js
+++ select.js
@@ -8,32 +8,43 @@ layui.define(['jquery', 'dropdown'],function(exports){
   var MOD_NAME = 'select';
 
   var select = {
-    onfig: {
-      keywordPlaceholder: '关键词'
+    // 默认配置
+    config: {
+      valueSeparator: ',',
+      keywordPlaceholder: '请输入关键词',
+      unfilteredText: '没有匹配的选项',
+      customName: {
+        id: 'id',
+        title: 'title',
+        selected: 'selected',
+      },
+      options: [],
+      collapseSelected: false,
     }
   };
 
   var thisSelect = function(){
     var that = this
-    ,options = that.config;
-    
+    ,config = that.config;
     
     return {
-      config: options
-      ,reload: function(options){
-        that.reload.call(that, options);
+      config: config
+      ,reload: function(config){
+        that.reload.call(that, config);
       },
+
+      // 获取值或设置值
       val: function(value) {
         if (!value) {
-          return that.selectedIds.join(',');
+          return that.context.selectedIds.join(config.valueSeparator);
         }
 
         that.clear.call(that);
 
-        var ids = $.isArray(value) ? value : value.split(',');
+        var ids = $.isArray(value) ? value : value.split(config.valueSeparator);
 
         for (var i = 0; i < ids.length; i++) {
-          var option = that.getDropdownData().filter(option => option.id == ids[i])[0];
+          var option = that.getOptionById(ids[i]);
           if (!option) {
             continue;
           }
@@ -47,194 +58,354 @@ layui.define(['jquery', 'dropdown'],function(exports){
     }
   }
 
-  var Class = function(options) {
+  var Class = function(config) {
     var that = this;
-    that.config = $.extend({}, that.config, select.config, options);
-    that.selectedIds = (options.value || []).map(function(option) {
-      return option.id;
+    
+    that.config = $.extend({}, that.config, select.config, config);
+
+    that.config.elem = $(config.elem);
+
+    that.config.elem.css({
+      position: 'absolute',
+      color: '#FFF',
+      userSelect: 'none',
+      top: 0,
+      height: '100%'
     });
-    that.dropdownOpened = false;
+
+    let width = that.config.elem.parent().outerWidth();
+
+    const layuiInputBlock = that.config.elem.parents('.layui-input-block');
+    if (layuiInputBlock.length > 0) {
+      width = layuiInputBlock.width();
+    }
+
+    const layuiInputGroup = that.config.elem.parents('.layui-input-group');
+    if (layuiInputGroup.length > 0) {
+      width -= layuiInputGroup.width();
+    }
+
+    // 将input包裹起来
+    that.config.elem.wrap(`<div class="layui-form-select multiple-select" style="width:${width}px"></div>`)
+    // 向下箭头
+    that.config.elem.after('<div class="layui-input-suffix"><i class="layui-edge"></i></div>');
 
     that.render();
   }
 
   Class.prototype.render = function() {
     var that = this;
-    var options = that.config;
+    var config = that.config;
 
-    options.elem = $(options.elem);
+    // 根据`config.customName`标准化
+    config.options = config.options.map(function(option) {
+      return {
+        id: option[that.config.customName.id],
+        title: option[that.config.customName.title],
+        selected: option[that.config.customName.selected],
+      };
+    });
 
+    // 上下文
+    this.context = {
+      // 搜索关键词
+      keyword: '',
+      // 满足搜索关键词的选项
+      filteredOptions: null,
+      // 已选择的ID
+      selectedIds: that.config.options.filter(function(option) {
+        return option.selected;
+      }).map(function(option) {
+        return option.id
+      }),
+      // dropdown滚动条的位置
+      dropdownMenuScrollTop: 0,
+    };
 
-    var temp  = '<div class="layui-form-select layui-form-select-multiple">';
-    temp += '<div class="layui-form-select-input">';
-    temp += `<input class="layui-input" lay-verify="${options.verify}" value="${that.selectedIds.join(',')}" name="${options.name}"><i class="layui-edge"></i>`;
-    temp += '</div>',
-    temp += '<div class="layui-form-select-options"></div>';
-    temp += '<div class="layui-form-select-placehoder">请选择</div>',
-    temp += '</div>'
+    // 赋值
+    config.elem.val(this.context.selectedIds.join(config.valueSeparator));
 
-    options.elem.html(temp);
+    // 因为样式层级问题，这里click相当于
+    config.elem.parent().on('click', '.multiple-select-selection', function() {
+      that.config.elem.click();
+    });
 
-    that.renderSelection();
-    that.renderDropdown();
-
-    // 移除选择
-    options.elem.find('.layui-form-select-options').on('click', '.layui-form-select-item-remove', function(e) {
+    // 移除选项
+    config.elem.parent().on('click', '.multiple-select-selection-item-remove', function(e) {
       e.stopPropagation();
-
-      var id = that.selectedIds[$(this).parent().index()],
-          option = that.getDropdownData().filter(option => option.id == id)[0];
+      var id = that.context.selectedIds[$(this).parent().index()],
+          option = that.getOptionById(id);
 
       that.remove(option);
-    })
+    });
+
+    that.renderDropdown();
+    that.renderSelection();
   };
 
-
+  // 清空所有值
   Class.prototype.clear = function() {
-    this.selectedIds = [];
-    this.reload();
+    this.context.selectedIds = [];
+    this.reloadDropdownData(this.buildRenderOptions());
   };
 
-
-  // 移除选择
+  // 移除选项
   Class.prototype.remove = function(option) {
-    this.selectedIds = this.selectedIds.filter(id => id != option.id);
-    this.reload();
+    this.context.selectedIds = this.context.selectedIds.filter(id => id != option.id);
+    this.reloadDropdownData(this.buildRenderOptions());
   };
 
 
-  // 选择
+  // 选择选项
   Class.prototype.select = function(option) {
-    this.selectedIds.push(option.id);
-    this.reload();
+    this.context.selectedIds.push(option.id);
+    this.reloadDropdownData(this.buildRenderOptions());
+  };
+
+
+  // 重置搜索
+  Class.prototype.resetSearch = function() {
+    this.context.filteredOptions = null;
+    this.context.keyword = '';
+
+    var input = this.getSearchInput();
+    input && input.val('');
   };
 
   // 重载
-  Class.prototype.reload = function(options) {
-    if (options) {
-      this.config = $.extend({}, this.config, options);
+  Class.prototype.reload = function(config) {
+    if (config) {
+      this.config = $.extend({}, this.config, config || {});
     }
-    
-    this.renderSelection();
-    this.dropdown.reload({content: this.getDropdownContent(), show: this.dropdownOpened});
-    var height = this.config.elem.find('.layui-form-select-options').height();
-    height = height > 35 ? height + 4 : height;
-    height = Math.max(height, 38);
-    this.config.elem.find('.layui-form-select-input > input').val(this.selectedIds.join(',')).css('height', height + 'px');
 
+    this.render();
   };
 
-  Class.prototype.getDropdownData = function() {
+  Class.prototype.buildRenderOptions = function(options) {
+    var that = this,
+        renderOptions = options || [];
+
+    if (renderOptions.length === 0) {
+      if (that.context.filteredOptions !== null) {
+        renderOptions = that.context.filteredOptions;
+      } else {
+        renderOptions = this.getAllOptions();
+      }
+    }
+
+    return renderOptions.map(function(option) {
+      option.selected = that.context.selectedIds.indexOf(option.id) !== -1;
+
+      return option;
+    });
+  };
+
+  // 过滤选项
+  Class.prototype.filterOptions = function(keyword) {
+    var that = this,
+        keyword = keyword.toLowerCase()
+      ;
+
+    return this.getAllOptions().filter(function(item) {
+      return -1 !== item.title.toLowerCase().indexOf(keyword);
+    });
+  };
+
+
+  // 获取所有选项
+  Class.prototype.getAllOptions = function() {
     var that = this;
-    return this.config.options.map(function(item) {
+
+    return that.config.options.map(function(item) {
       return {
         id: item.id,
         title: item.title,
-        checked: that.selectedIds.indexOf(item.id) !== -1
+        selected: that.context.selectedIds.indexOf(item.id) !== -1
       }
     });
   };
 
-  Class.prototype.getDropdownContent = function() {
+  
+  Class.prototype.buildDropdownContent = function(options) {
     return [
-        '<div class="layui-form-select-search"><input class="layui-input" autofocus placeholder="关键词"/></div>',
-        '<ul class="layui-menu layui-dropdown-menu">',
-        this.getDropdownData().map(function(option) {
-          return `<li><div class="layui-menu-body-title"><div class="layui-form-select-dropdown-item ${option.checked ? 'layui-form-select-dropdown-selected' : ''}"><i class="layui-icon layui-icon-ok"></i>${option.title}</div></div></li>`;
-        }).join(''),
+        `<div class="multiple-select-search"><input class="layui-input" placeholder="${this.config.keywordPlaceholder}"/></div>`,
+        '<ul class="layui-menu layui-dropdown-menu" style="max-height: 300px;overflow-y: auto;">',
+        options.length > 0 ? options.map(function(option) {
+          return `<li data-value="${option.id}" class="multiple-select-option ${option.selected ? 'multiple-select-option-selected' : ''}">${option.title}</li>`;
+        }).join('') : `<div style="padding: 5px;font-size:12px;">${this.config.unfilteredText}</div>`,
         '</ul>',
       ].join('');
   };
 
+
+  Class.prototype.reloadDropdownData = function(options) {
+    if (options && options.length === this.getAllOptions().length) {
+      this.resetSearch();
+    }
+
+    var renderOptions = this.buildRenderOptions(options);
+
+    this.dropdown.reloadData({
+      content: this.buildDropdownContent(renderOptions)
+    });
+
+    this.renderSelection();
+  };
+
+  Class.prototype.getSearchInput = function() {
+    if (!this.panel) {
+      return ;
+    }
+
+    return this.panel.find('div.multiple-select-search > input');
+  };
+
+
   Class.prototype.renderDropdown = function() {
     var that = this;
 
-    that.dropdownOpened = false;
-
-    var panel = dropdown.render({
+    that.dropdown = dropdown.render({
       elem: that.config.elem,
-      style: 'width:' + that.config.elem.width() + 'px',
-      content: that.getDropdownContent(),
-      templet: function(d) {
-        return `<div class="layui-form-select-dropdown-item ${d.checked ? 'layui-form-select-dropdown-selected' : ''}"><i class="layui-icon layui-icon-ok"></i>${d.title}</div>`
-      },
+      style: 'width:' + that.config.elem.outerWidth() + 'px',
+      content: that.buildDropdownContent(that.getAllOptions()),
       ready: function(panel, elem) {
-        that.dropdownOpened = true;
-        // select箭头
-        that.config.elem.find('.layui-form-select').addClass('layui-form-select-panel-opened');
+        that.panel = panel;
+
+        elem.parent().addClass('multiple-select-panel-opended')
+
+        // 保持滚动条位置
+        if (that.context.dropdownMenuScrollTop > 0) {
+          panel.find('.layui-dropdown-menu').scrollTop(that.context.dropdownMenuScrollTop);
+        }
+
+        var inputCompositionStart = false,
+            searchInput = that.getSearchInput();
+
+        function reloadDropdown(keyword) {
+          that.context.keyword = keyword;
+          that.context.filteredOptions = that.filterOptions(keyword);
+          that.reloadDropdownData(that.context.filteredOptions);
+        }
 
         // 搜索框事件
-        panel.find('div.layui-form-select-search > input').on('input', function() {
-          var keyword = $(this).val().toLowerCase();
-          panel.find('ul.layui-dropdown-menu li').each(function() {
-            // 不匹配则隐藏
-            if ($(this).text().toLowerCase().indexOf(keyword) === -1) {
-              $(this).hide();
-            } else {
-              $(this).show();
-            }
-          })
+        searchInput.on('input', function() {
+          if (inputCompositionStart) {
+            return ;
+          }
+
+          reloadDropdown($(this).val());
+        }).on('compositionstart', function() {
+          // compositionstart compositionend是为了支持中文类字母
+          inputCompositionStart = true;
+        }).on('compositionend', function() {
+          inputCompositionStart = false;
+          reloadDropdown($(this).val());
         });
 
+        // 每次输入都会重新渲染一次dropdown，所以要记已上次输入
+        if (that.context.keyword !== '') {
+          searchInput.val(that.context.keyword).focus();
+        }
+
+        // 检测dropdown的面版是否已关闭
         var i = setInterval(function() {
           // 是否已展开
           if (!elem.data('layui_dropdown_index_opened')) {
-            that.dropdownOpened = false;
-
-            that.config.elem.find('.layui-form-select').removeClass('layui-form-select-panel-opened');
             clearInterval(i);
+
+            that.context.keyword = '';
+            that.context.filteredOptions = null;
+            that.reloadDropdownData();
+
+            elem.parent().removeClass('multiple-select-panel-opended')
           }
         }, 100);
       },
 
       click: function(data, elem) {
         // 获取点击项的索引
-        var index = elem.index(),
-            selectedOption = that.getDropdownData()[index]; 
-        
-        // 如果值不存在
-        if (that.selectedIds.indexOf(selectedOption.id) === -1) {
-          that.select(selectedOption);
+        var value = elem.data('value'),
+            option = that.getOptionById(value)
+            ;
+
+        if (!option) {
+          return ;
+        }
+
+        // 记录选择面板滚动的位置
+        that.context.dropdownMenuScrollTop = elem.parent().scrollTop();
+
+        if (that.context.selectedIds.indexOf(option.id) === -1) {
+          that.select(option);
         } else {
-          that.remove(selectedOption);
+          that.remove(option);
         }
 
         // false不关闭面板
         return false;
       }
     });
+  };
 
-    that.dropdown = panel;
+  // 根据ID获取选项
+  Class.prototype.getOptionById = function(id) {
+    return this.getAllOptions().filter(function(option) {
+      return option.id == id;
+    })[0];
   };
 
 
+  // 渲染选择
   Class.prototype.renderSelection = function() {
-    var tpl = [
-        '{{#  layui.each(d, function(index, option) { }}',
-        '<div class="layui-inline layui-form-select-selection-item" data-id="{{= option.id }}">',
-        '<span class="layui-form-select-selection-item-content">{{= option.title}}</span>',
-        '<i class="layui-icon layui-icon-close layui-form-select-item-remove"></i></div>',
-        '{{# }) }}',
-      ].join('')
-      ,selectPlacehodlerElem = this.config.elem.find('div.layui-form-select-placehoder')
-      ,h = laytpl(tpl).render(this.getSelectedOptions());
+    var that = this,
+        inputWrap = that.config.elem.parent(),
+        selectedOptions = that.getSelectedOptions(),
+        options = selectedOptions.length > 1 && that.config.collapseSelected ? [selectedOptions[0]] : selectedOptions,
+        selectionHtml = '',
+        selectionTpl = `
+          <div class="multiple-select-selection">
+            <div class="multiple-select-selection-overflow">
+              {{# layui.each(d.options, function(index, option) { }}
+                <div class="multiple-select-selection-overflow-item">
+                  <span class="multiple-select-selection-item" data-id="{{= option.id }}">
+                    <span class="multiple-select-selection-item-content">{{= option.title}}</span>
+                    <i class="layui-icon layui-icon-close multiple-select-selection-item-remove"></i>
+                  </span>
+                </div>
+              {{# }) }}
+              {{# if (d.collapseSelected && d.total > 1) { }}
+                <div class="multiple-select-selection-overflow-item">
+                  <span class="multiple-select-selection-item">
+                    <span class="multiple-select-selection-item-content">+ {{ d.total }}</span>
+                  </span>
+                </div>
+              {{# } }}
+            </div>
+          </div>
+        `
+        ;
 
-    this.config.elem.find('div.layui-form-select-options').html(h);
-
-    if (this.selectedIds.length === 0) {
-      selectPlacehodlerElem.show();
-    } else {
-      selectPlacehodlerElem.hide();
+    if (selectedOptions.length > 0) {
+      selectionHtml = laytpl(selectionTpl).render({
+        total: selectedOptions.length,
+        options: options,
+        collapseSelected: that.config.collapseSelected
+      });
     }
+
+    inputWrap.find('.multiple-select-selection').remove();
+    selectionHtml !== '' && inputWrap.append(selectionHtml);
+
+    this.config.elem.val(this.context.selectedIds.join(this.context.valueSeparator));
   };
 
   Class.prototype.getSelectedOptions = function() {
     var that = this;
-    return that.config.options.filter(function(item) {
-      return that.selectedIds.indexOf(item.id) !== -1
+
+    return that.context.selectedIds.map(function(id) {
+      return that.getOptionById(id);
     });
   };
-
 
 
   //核心入口
